ea0edb53dfd193ce73bc6ecd92979c2c0df7f83c
Sergey Shishkin <sergey@shishkin.org>
2026-04-16 16:34:08 +0200

    feat: retrying pending client updates

--- packages/y-sse/src/index.ts
+++ packages/y-sse/src/index.ts
@@ -21,21 +21,25 @@ export class SseProvider {
   readonly awareness: awarenessProtocol.Awareness;
   readonly docId: string;
   readonly pathPrefix: string;
+  readonly docPath: string;
   private stream: EventSource;
-  private session: Promise<EventPayloadMap["init"]>;
+  private sessionPath: Promise<string>;
+  private pendingUpdate: Uint8Array | undefined;
+  private updateAttempt = 0;
 
   constructor({ doc, docId, pathPrefix = "/sse", awareness = {} }: ClientOptions) {
     this.doc = doc;
     this.docId = docId;
     this.pathPrefix = pathPrefix;
 
-    const docPath = `${this.pathPrefix}/${this.docId}`;
-    this.stream = new EventSource(docPath);
-    this.session = new Promise((resolve) => {
+    this.docPath = `${this.pathPrefix}/${this.docId}`;
+    this.stream = new EventSource(this.docPath);
+    this.sessionPath = new Promise((resolve) => {
       this.stream.addEventListener(
         "init",
         (e) => {
-          resolve(JSON.parse(e.data));
+          const payload = JSON.parse(e.data) as EventPayloadMap["init"];
+          resolve(`${this.docPath}/${payload.session}`);
         },
         { once: true },
       );
@@ -46,14 +50,10 @@ export class SseProvider {
       Y.applyUpdate(this.doc, update);
     });
     this.doc.on("update", async (update) => {
-      try {
-        const { session } = await this.session;
-        const sessionPath = `${docPath}/${session}`;
-        await this.postUpdate(sessionPath, update);
-      } catch (e) {
-        // TODO: implement pending updates queue
-        console.error(e);
-      }
+      this.pendingUpdate = this.pendingUpdate
+        ? Y.mergeUpdates([this.pendingUpdate, update])
+        : update;
+      this.tryPostingPendingUpdate();
     });
 
     this.awareness = new awarenessProtocol.Awareness(doc);
@@ -74,8 +74,8 @@ export class SseProvider {
         removed: number[];
       }) => {
         try {
-          const { session } = await this.session;
-          const awarenessPath = `${docPath}/${session}?awareness`;
+          const path = await this.sessionPath;
+          const awarenessPath = `${path}?awareness`;
           const changed = added.concat(updated).concat(removed);
           const update = awarenessProtocol.encodeAwarenessUpdate(this.awareness, changed);
           await this.postUpdate(awarenessPath, update);
@@ -87,6 +87,26 @@ export class SseProvider {
     window.addEventListener("beforeunload", () => {
       awarenessProtocol.removeAwarenessStates(this.awareness, [this.doc.clientID], "window unload");
     });
+  }
+
+  private async tryPostingPendingUpdate() {
+    if (this.updateAttempt) {
+      // loop is already running
+      return;
+    }
+    while (this.pendingUpdate) {
+      try {
+        this.updateAttempt++;
+        const path = await this.sessionPath;
+        await this.postUpdate(path, this.pendingUpdate);
+        this.pendingUpdate = undefined;
+        this.updateAttempt = 0;
+      } catch (e) {
+        console.error(e);
+        const delay = Math.min(500 * Math.pow(2, Math.max(this.updateAttempt - 1, 0)), 30_000);
+        await new Promise((resolve) => setTimeout(resolve, delay));
+      }
+    }
   }
 
   private async postUpdate(path: string, update: Uint8Array) {
